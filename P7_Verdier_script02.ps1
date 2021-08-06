bb62da8aaaf6fcca03b43eba25e74d614ab13349
Justin Verdier <justin.verdier@gmail.com>
2021-08-06 16:48:13 +0200

    script 2 fonctionnel

---
+++ P7_Verdier_script02.ps1
@@ -0,0 +1,26 @@
+﻿# Lister les membres d'un groupe
+
+
+$groupe=$args[0]
+
+if ( $groupe -isnot [string] )
+{
+        $groupe=read-host "Merci de préciser le groupe que vous voulez voir"
+}
+
+$dossier=$args[1]
+
+if ( $dossier -isnot [string] )
+{
+        $dossier=read-host "Merci d'indiquer le chemin de sauvegarde"
+}
+
+try
+{
+    Get-ADGroupMember -identity $groupe | select-object name | Export-Csv membres.csv -Encoding UTF8
+    write-host "Votre fichier a bien été créé"
+}
+catch
+{
+    write-host "Impossible d'afficher les membres de ce groupe"
+}
