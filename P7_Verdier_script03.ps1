23f0cd2c51bc510e3a24badba7063c95e30f8a25
Justin Verdier <justin.verdier@gmail.com>
2021-08-10 11:30:45 +0200

    script3 première version

---
+++ P7_Verdier_script03.ps1
@@ -0,0 +1,19 @@
+﻿# Liste des groupes dont un utilisateur est membre
+
+
+$user=$args[1]
+
+if ( $user -isnot [string] )
+{
+        $user=read-host "Merci d'indiquer le nom d'utilisateur"
+}
+
+try
+{
+    Get-ADUser -Identity $user -Properties Memberof | Select-Object Memberof "OU=,DC=acme,DC=fr" | Out-File -Filepath "C:\Users\Admnistrateur\Desktop\usergroupmember.csv" -Encoding UTF8
+    write-host "L'utilisateur fait partie de ces groupes:"
+}
+catch
+{
+    write-host "Impossible d'afficher les groupes dont l'utilisateur fait partie"
+}
