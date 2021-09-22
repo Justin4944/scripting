65e08ec09b967d123a5f9cace9146080e503824f
justin4944 <justin.verdier@gmail.com>
2021-09-22 18:19:50 +0200

    script 4 ok

--- P7_Verdier_script04.ps1
+++ P7_Verdier_script04.ps1
@@ -1,15 +1,19 @@
+
 $Listpc=(Get-ADComputer -Filter 'Name -like "DESKTOP*"').name
 Foreach ($line in $Listpc)
 {
                
-$Listuser=(Get-ADUser -Filter * -searchbase "DC=ACME,DC=FR").SamAccountName
-
-
+    $Listuser=(Get-ADUser -Filter * -searchbase "DC=ACME,DC=FR").SamAccountName
+    
 
     Foreach($user in $Listuser)
 
-{
-        New-Item -Path "C:\SAV\$user" -ItemType directory
-        xcopy "\\$line\$user\" "\\SRVWIN01\SAV\$user"
+{   
+
+        copy-item -Path "\\$line\Users\$user\Documents" -Destination "C:\SAV\$line\$user\Documents" -Force -Recurse       
 }
 }
+  
+   
+ # xcopy "\\$line\Users\$user" "C:\SAV\$line\$user" /e   
+ # New-Item -Path "C:\SAV\$line\$user" -ItemType directory
